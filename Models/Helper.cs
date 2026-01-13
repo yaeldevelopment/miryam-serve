365f7b76a1d82610750a0cd433bd72ac5c2faa9b
yael <y0556722091@gmail.com>
2026-01-13 15:25:38 +0200

    email

--- Models/Helper.cs
+++ Models/Helper.cs
@@ -1,43 +1,35 @@
 ﻿using System.Net;
 using System.Net.Mail;
+using System.Text;
 
 namespace server.Models
 {
     public class Helper
     {
-        public static async Task SendEmailAsync(string toEmail, string subject, string body)
+        public static async Task SendEmailAsync( string subject, string html)
         {
-            try
+            var apiKey = Environment.GetEnvironmentVariable("RESEND_API_KEY");
+
+            using var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", $"Bearer {apiKey}");
+
+            var payload = new
             {
-                var fromAddress = new MailAddress("tripyaeleden@gmail.com", "פניית לקוח");
-                var toAddress = new MailAddress("y0556722091@gmail.com");
-                string fromPassword = "urof ehie vwrt bxmp"; // **וודאי שאת משתמשת בסיסמת אפליקציה**
+                from = "onboarding@resend.dev",
+                to = new[] {  Environment.GetEnvironmentVariable("to_email") },
+                subject = subject,
+                html = html
+            };
 
-                var smtp = new SmtpClient
-                {
-                    Host = "smtp.gmail.com",
-                    Port = 587,
-                    EnableSsl = true,          
-                    Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
-                };
+            var content = new StringContent(
+                System.Text.Json.JsonSerializer.Serialize(payload),
+                Encoding.UTF8,
+                "application/json"
+            );
 
-                using (var message = new MailMessage(fromAddress, toAddress))
-                {
-                    message.Subject = subject;
-                    message.Body = body;
-                    message.IsBodyHtml = true;
+            var response = await client.PostAsync("https://api.resend.com/emails", content);
+            response.EnsureSuccessStatusCode();
+        }
 
-
-                    await smtp.SendMailAsync(message);
-                    Console.WriteLine("✅ המייל נשלח בהצלחה!");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"❌ שגיאה בשליחת המייל: {ex.Message}");
-                throw new Exception($"❌ שגיאה בשליחת המייל: {ex.ToString()}");
-            }
-      
-    }
     }
 }
